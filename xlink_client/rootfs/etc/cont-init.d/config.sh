b53c99adee65f837bdcb3017e8d00c77e54a256f
Felix Nölte <mail@felixnoelte.de>
2026-01-01 16:15:11 +0000

    wip

--- xlink_client/rootfs/etc/cont-init.d/config.sh
+++ xlink_client/rootfs/etc/cont-init.d/config.sh
@@ -29,6 +29,7 @@ declare server_private_key
 declare server_public_key
 declare table
 
+# todo: use different directory, but make sure it is persisted
 if ! bashio::fs.directory_exists '/ssl/wireguard'; then
     mkdir -p /ssl/wireguard ||
         bashio::exit.nok "Could not create wireguard storage folder!"
@@ -41,121 +42,155 @@ if bashio::config.has_value "server.interface"; then
 fi
 config="/etc/wireguard/${interface}.conf"
 
-# Start creation of configuration
-echo "[Interface]" > "${config}"
-
-# Check if at least 1 address is specified
-if ! bashio::config.has_value 'server.addresses'; then
-    bashio::exit.nok 'You need at least 1 address configured for the server'
+# Check if hostname is specified
+if ! bashio::config.has_value 'gateway.host'; then
+    bashio::exit.nok 'You need to specifiy the hostname of the remote gateway'
 fi
+gateway_host=$(bashio::config "gateway.host")
+# Check if port is specified
+if ! bashio::config.has_value 'gateway.port'; then
+    bashio::exit.nok 'You need to specifiy the port of the remote gateway'
+fi
+gateway_port=$(bashio::config "gateway.port")
+# Check if gateway public key is specified
+if ! bashio::config.has_value 'gateway.public_key'; then
+    bashio::exit.nok 'You need to specifiy the public key of the remote gateway'
+fi
+gateway_public_key=$(bashio::config "gateway.public_key")
 
-# Add all server addresses to the configuration
-for address in $(bashio::config 'server.addresses'); do
-    [[ "${address}" == *"/"* ]] || address="${address}/24"
-    echo "Address = ${address}" >> "${config}"
-done
+gateway_allowed_ips="10.0.0.1/32"
+gateway_keepalive=5
+
+# Write gateway=peer to configuration file
+{
+    echo "[Peer]"
+    echo "PublicKey = ${gateway_public_key}"
+    echo "Endpoint = ${gateway_host}:${gateway_port}"
+    echo "AllowedIPs = ${gateway_allowed_ips}"
+    echo "PersistentKeepalive = ${gateway_keepalive}"
+    echo ""
+} >> "${config}"
+
+######### INTERFACE = CLIENT
+
+# todo: generate private key??
+# todo:Add private key to client??
+
+client_private_key=$(bashio::config "client.private_key")
+# Check if client private key is specified
+if ! bashio::config.has_value 'client.private_key'; then
+    bashio::exit.nok 'You need to specifiy the client key'
+fi
+client_private_key=$(bashio::config "client.private_key")
+
+client_addr="10.0.0.2/24"
+
+# Write Client=interface to configuration file
+{
+    echo "[Interface]"
+    echo "Address = ${client_addr}"
+    echo "PrivateKey = ${client_private_key}"
+} >> "${config}"
 
 # Add all server DNS addresses to the configuration
-if bashio::config.has_value 'server.dns'; then
-    for dns in $(bashio::config 'server.dns'); do
-        echo "DNS = ${dns}" >> "${config}"
-    done
-else
-    dns=$(bashio::dns.host)
-    echo "DNS = ${dns}" >> "${config}"
-fi
+#if bashio::config.has_value 'server.dns'; then
+#    for dns in $(bashio::config 'server.dns'); do
+#        echo "DNS = ${dns}" >> "${config}"
+#    done
+#else
+#    dns=$(bashio::dns.host)
+#    echo "DNS = ${dns}" >> "${config}"
+#fi
 
 # Get the server's private key
-if bashio::config.has_value 'server.private_key'; then
-    server_private_key=$(bashio::config 'server.private_key')
-else
-    if ! bashio::fs.file_exists '/ssl/wireguard/private_key'; then
-        umask 077 || bashio::exit.nok "Could not set a proper umask"
-        wg genkey > /ssl/wireguard/private_key ||
-            bashio::exit.nok "Could not generate private key!"
-    fi
-    server_private_key=$(</ssl/wireguard/private_key)
-fi
+#if bashio::config.has_value 'server.private_key'; then
+#    server_private_key=$(bashio::config 'server.private_key')
+#else
+#    if ! bashio::fs.file_exists '/ssl/wireguard/private_key'; then
+#        umask 077 || bashio::exit.nok "Could not set a proper umask"
+#        wg genkey > /ssl/wireguard/private_key ||
+#            bashio::exit.nok "Could not generate private key!"
+#    fi
+#    server_private_key=$(</ssl/wireguard/private_key)
+#fi
 
 # Get the server pubic key
-if bashio::config.has_value 'server.public_key'; then
-    server_public_key=$(bashio::config 'server.public_key')
-else
-    server_public_key=$(wg pubkey <<< "${server_private_key}")
-fi
+#if bashio::config.has_value 'server.public_key'; then
+#    server_public_key=$(bashio::config 'server.public_key')
+#else
+#    server_public_key=$(wg pubkey <<< "${server_private_key}")
+#fi
 
-fwmark=$(bashio::config "server.fwmark")
-mtu=$(bashio::config "server.mtu")
-pre_down=$(bashio::config "server.pre_down")
-pre_up=$(bashio::config "server.pre_up")
-table=$(bashio::config "server.table")
+#fwmark=$(bashio::config "server.fwmark")
+#mtu=$(bashio::config "server.mtu")
+#pre_down=$(bashio::config "server.pre_down")
+#pre_up=$(bashio::config "server.pre_up")
+#table=$(bashio::config "server.table")
 
 # Pre Up & Down handling
-if [[ "${pre_up}" = "off" ]]; then
-    pre_up=""
-fi
-if [[ "${pre_down}" = "off" ]]; then
-    pre_down=""
-fi
+#if [[ "${pre_up}" = "off" ]]; then
+#    pre_up=""
+#fi
+#if [[ "${pre_down}" = "off" ]]; then
+#    pre_down=""
+#fi
 
 # Post Up & Down defaults
-post_up="iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE"
-post_down="iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE"
-if [[ $(</proc/sys/net/ipv4/ip_forward) -eq 0 ]]; then
-    bashio::log.warning
-    bashio::log.warning "IP forwarding is disabled on the host system!"
-    bashio::log.warning "You can still use WireGuard to access Hass.io,"
-    bashio::log.warning "however, you cannot access your home network or"
-    bashio::log.warning "the internet via the VPN tunnel."
-    bashio::log.warning
-    bashio::log.warning "Please consult the add-on documentation on how"
-    bashio::log.warning "to resolve this."
-    bashio::log.warning
+#post_up="iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE"
+#post_down="iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE"
+#if [[ $(</proc/sys/net/ipv4/ip_forward) -eq 0 ]]; then
+#    bashio::log.warning
+#    bashio::log.warning "IP forwarding is disabled on the host system!"
+#    bashio::log.warning "You can still use WireGuard to access Hass.io,"
+#    bashio::log.warning "however, you cannot access your home network or"
+#    bashio::log.warning "the internet via the VPN tunnel."
+#    bashio::log.warning
+#    bashio::log.warning "Please consult the add-on documentation on how"
+#    bashio::log.warning "to resolve this."
+#    bashio::log.warning
 
     # Set fake placeholders for Up & Down commands
-    post_up=""
-    post_down=""
-fi
+#    post_up=""
+#    post_down=""
+#fi
 
 # Load custom PostUp setting if provided
-if bashio::config.has_value 'server.post_up'; then
-    post_up=$(bashio::config 'server.post_up')
-    if [[ "${post_up}" = "off" ]]; then
-        post_up=""
-    fi
-fi
+#if bashio::config.has_value 'server.post_up'; then
+#    post_up=$(bashio::config 'server.post_up')
+#    if [[ "${post_up}" = "off" ]]; then
+#        post_up=""
+#    fi
+#fi
 
 # Load custom PostDown setting if provided
-if bashio::config.has_value 'server.post_down'; then
-    post_down=$(bashio::config 'server.post_down')
-    if [[ "${post_down}" = "off" ]]; then
-        post_down=""
-    fi
-fi
+#if bashio::config.has_value 'server.post_down'; then
+#    post_down=$(bashio::config 'server.post_down')
+#    if [[ "${post_down}" = "off" ]]; then
+#        post_down=""
+#    fi
+#fi
 
 # Finish up the main server configuration
 {
-    echo "PrivateKey = ${server_private_key}"
-
     # Adds server port to the configuration
-    echo "ListenPort = 51820"
+    #echo "ListenPort = 51820"
 
     # Custom routing table
-    bashio::config.has_value "server.table" && echo "Table = ${table}"
+    # bashio::config.has_value "server.table" && echo "Table = ${table}"
 
     # Pre up & down
-    bashio::config.has_value "server.pre_up" && echo "PreUp = ${pre_up}"
-    bashio::config.has_value "server.pre_down" && echo "PreDown = ${pre_down}"
+    # bashio::config.has_value "server.pre_up" && echo "PreUp = ${pre_up}"
+    # bashio::config.has_value "server.pre_down" && echo "PreDown = ${pre_down}"
 
     # Post up & down
-    bashio::var.has_value "${post_up}" && echo "PostUp = ${post_up}"
-    bashio::var.has_value "${post_down}" && echo "PostDown = ${post_down}"
+    # bashio::var.has_value "${post_up}" && echo "PostUp = ${post_up}"
+    # bashio::var.has_value "${post_down}" && echo "PostDown = ${post_down}"
 
     # fwmark for outgoing packages
-    bashio::config.has_value "server.fwmark" && echo "FwMark = ${fwmark}"
+    #bashio::config.has_value "server.fwmark" && echo "FwMark = ${fwmark}"
 
     # Custom MTU setting
-    bashio::config.has_value "server.mtu" && echo "MTU = ${mtu}"
+    # bashio::config.has_value "gateway.mtu" && echo "MTU = ${mtu}"
 
     # End configuration file with an empty line
     echo ""
